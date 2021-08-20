0bb80fc8e7020c745825f33c318a5bc5ef9ece10
Iain <iain@zora.co>
2021-08-20 13:22:27 -0400

    fix index types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,37 @@
-import styled from '@emotion/styled'
-import Head from '../components/head'
-import { PageWrapper } from '../styles/components'
-import { GetStaticProps } from 'next'
-import { fetchTokens } from '../data/fetchTokens'
+import styled from "@emotion/styled";
+import Head from "../components/head";
+import { PageWrapper } from "../styles/components";
+import { GetStaticProps } from "next";
+import { fetchTokens } from "../data/fetchTokens";
 
-import { AuctionsList } from '../components/AuctionsList'
-import { FetchStaticData, MediaFetchAgent, NetworkIDs } from '@zoralabs/nft-hooks'
+import { AuctionsList } from "../components/AuctionsList";
+import {
+  FetchStaticData,
+  MediaFetchAgent,
+  NetworkIDs,
+} from "@zoralabs/nft-hooks";
 
 export default function Home({ tokens }: { tokens: any }) {
-  console.log(tokens)
+  console.log(tokens);
   return (
     <IndexWrapper>
-      <Head/>
+      <Head />
       <h1>{process.env.NEXT_PUBLIC_APP_TITLE}</h1>
       <AuctionsList tokens={tokens} />
     </IndexWrapper>
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const fetchAgent = new MediaFetchAgent(process.env.NEXT_PUBLIC_NETWORK_ID as NetworkIDs);
+  const fetchAgent = new MediaFetchAgent(
+    process.env.NEXT_PUBLIC_NETWORK_ID as NetworkIDs
+  );
   const tokens = await FetchStaticData.fetchZoraIndexerList(fetchAgent, {
     curatorAddress: process.env.NEXT_PUBLIC_CURATORS_ID as string,
-    collectionAddress: process.env.NEXT_PUBLIC_TARGET_CONTRACT_ADDRESS as string,
+    collectionAddress: process.env
+      .NEXT_PUBLIC_TARGET_CONTRACT_ADDRESS as string,
     limit: 100,
+    offset: 0,
   });
 
   return {
@@ -36,4 +44,4 @@ export const getStaticProps: GetStaticProps = async () => {
 
 const IndexWrapper = styled(PageWrapper)`
   max-width: var(--content-width-xl);
-`
+`;
