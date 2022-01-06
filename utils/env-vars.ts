31b6552d625911ee5ee098b11eac6fb5539bae84
Iain <iain@zora.co>
2022-01-06 14:11:58 -0500

    switch to negative and condition

--- utils/env-vars.ts
+++ utils/env-vars.ts
@@ -5,7 +5,7 @@ if (!NETWORK_ID) {
 
 export const CURATOR_ID = process.env.NEXT_PUBLIC_CURATOR_ID;
 export const CONTRACT_ADDRESSES = process.env.NEXT_PUBLIC_CONTRACT_ADDRESSES;
-if (!CURATOR_ID || !CONTRACT_ADDRESSES) {
+if (!CURATOR_ID && !CONTRACT_ADDRESSES) {
   throw new Error(
     "At least one of curator id or contract address is required"
   );
