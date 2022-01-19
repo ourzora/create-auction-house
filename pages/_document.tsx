97f36ad72dc5d56a50aa89ef7e93f68ccb94b3b6
mindsgn <seni.tembe@protonmail.com>
2022-01-19 14:46:10 +0200

    crossOrigin fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ class CreateAuctionHouseDocument extends Document {
         <Html>
           <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@300;400;900&display=swap" rel="stylesheet"/>
           </Head>
           <body>
