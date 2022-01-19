292f194f6c659856da4c893e79a3e1dcaf44bafe
mindsgn <seni.tembe@protonmail.com>
2022-01-19 14:40:35 +0200

    header link fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,9 @@ class CreateAuctionHouseDocument extends Document {
       <>
         <Html>
           <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@300;400;900&display=swap" rel="stylesheet"/>
           </Head>
           <body>
             <Main />
